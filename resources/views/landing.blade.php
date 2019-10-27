bea1bd3632df625e6f4345fb041ffb9210c0e035
Your Name <you@example.com>
2019-10-27 21:19:37 +0100

    Improve landing page

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -1,17 +1,151 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">SGSI</div>
+<head>
 
-                <div class="card-body">
-                    <h1>Sistema de gestión de riesgos basado en MAGERIT</h1>
-                </div>
-            </div>
-        </div>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>{{ config('app.name', 'Planus') }} - Sistema de Análisis y Gestión de Riesgos</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+
+  <!-- Custom fonts for this template -->
+  <link href="{{ asset('fontawesome-free/css/all.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('simple-line-icons/css/simple-line-icons.css') }}" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+
+  <!-- Custom styles for this template -->
+  <link href="{{ asset('css/landing-page.min.css') }}" rel="stylesheet">
+</head>
+<body>
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-light bg-light static-top">
+    <div class="container">
+      <a class="navbar-brand" href="#">{{ config('app.name', 'Planus') }}</a>
+
+      @guest
+        <a class="btn btn-primary" href="{{ route('login') }}">{{ __('Login') }}</a>
+      @endguest
     </div>
-</div>
-@endsection
+  </nav>
+
+  <!-- Masthead -->
+  <header class="masthead text-white text-center">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-xl-12 mx-auto">
+          <h1 class="mb-5">Sistema de Análisis y Gestión de Riesgos de los Sistemas de Información</h1>
+        </div>
+        <div class="col-md-10 col-lg-8 col-xl-7 mx-auto">
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <!-- Icons Grid -->
+  <section class="features-icons bg-light text-center">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-4">
+          <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+            <div class="features-icons-icon d-flex">
+              <i class="icon-screen-desktop m-auto text-primary"></i>
+            </div>
+            <h3>Gestionar Activos</h3>
+            <p class="lead mb-0">Identificar los activos más importantes de la organización</p>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+            <div class="features-icons-icon d-flex">
+              <i class="icon-ghost m-auto text-primary"></i>
+            </div>
+            <h3>Gestionar Riesgos</h3>
+            <p class="lead mb-0">Organizar la defensa de los activos críticos</p>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="features-icons-item mx-auto mb-0 mb-lg-3">
+            <div class="features-icons-icon d-flex">
+              <i class="icon-fire m-auto text-primary"></i>
+            </div>
+            <h3>Identificar amenazas</h3>
+            <p class="lead mb-0">Determinar que amenaza pueda afectar a cada activo</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Image Showcases -->
+  <section class="showcase">
+    <div class="container-fluid p-0">
+      <div class="row no-gutters">
+
+        <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('img/bg-showcase-1.jpg');"></div>
+        <div class="col-lg-6 order-lg-1 my-auto showcase-text">
+          <h2>Gestionar Activos</h2>
+          <p class="lead mb-0">Organizar fácilmente la información sobre los activos relevantes de la organización.</p>
+        </div>
+      </div>
+      <div class="row no-gutters">
+        <div class="col-lg-6 text-white showcase-img" style="background-image: url('img/bg-showcase-2.jpg');"></div>
+        <div class="col-lg-6 my-auto showcase-text">
+          <h2>Gestionar Riesgos</h2>
+          <p class="lead mb-0">Identificar los activos vulnerables ordenados por criticidad, visualizar las amenazas y salvaguardas posibles para cada activo.</p>
+        </div>
+      </div>
+      <div class="row no-gutters">
+        <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('img/bg-showcase-3.jpg');"></div>
+        <div class="col-lg-6 order-lg-1 my-auto showcase-text">
+          <h2>Identificar amenazas</h2>
+          <p class="lead mb-0">Visualizar las amenazas disponibles por criticidad y probabilidad de ocurrencia.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Call to Action -->
+  <section class="call-to-action text-white text-center">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-xl-9 mx-auto">
+          <h2 class="mb-4">¿Listo para empezar? Registrese ahora</h2>
+        </div>
+        <div class="col-md-10 col-lg-8 col-xl-7 mx-auto">
+          <form action="{{ route('register') }}">
+            <div class="form-row">
+              <div class="col-12 col-md-3 mx-auto">
+                <button type="submit" class="btn btn-block btn-lg btn-primary">{{ __('Registrarme') }}</button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Footer -->
+  <footer class="footer bg-light">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 h-100 text-center text-lg-left my-auto">
+          <p class="text-muted small mb-4 mb-lg-0">&copy; Planus {{ date("Y") }}. All Rights Reserved.</p>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
